df64c6684e1b117e146ae9ba601fd38264685804
Taylor Smits <taylor.smits@villagemd.com>
2020-03-27 11:54:59 -0500

    add intellij-idea

--- brew.sh
+++ brew.sh
@@ -20,6 +20,7 @@ brew cask install iterm2
 brew cask install postman
 brew cask install visual-studio-code
 brew cask install spotify
+brew cask install intellij-idea
 
 # Remove outdated versions from the cellar.
 brew cleanup
