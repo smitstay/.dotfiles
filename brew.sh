bb2eb0b280d16aee65a8865037f313fa7186eee0
Taylor Smits <taylor.smits@villagemd.com>
2020-05-11 17:15:46 -0500

    brew and mac updates

--- brew.sh
+++ brew.sh
@@ -14,13 +14,19 @@ brew cask install font-fira-code
 # apps
 brew install pyenv
 brew install node
+brew install jq
+brew cask install ngrok
 brew cask install clipy
 brew cask install docker
 brew cask install iterm2
+brew cask install the-unarchiver
 brew cask install postman
 brew cask install visual-studio-code
 brew cask install spotify
 brew cask install intellij-idea-ce
-
+brew tap adoptopenjdk/openjdk
+brew cask install adoptopenjdk8
+# mssql driver
+brew install freetds 
 # Remove outdated versions from the cellar.
 brew cleanup
